81a4969626f3da44e0e12bd6a13e210e0819a7bf
Felipe Acevedo <facevedom@unal.edu.co>
2016-09-10 23:13:09 -0500

    Interfaz cambiar contraseña

--- app/src/main/java/com/example/juangui/un_app/CreateAccountActivity.java
+++ app/src/main/java/com/example/juangui/un_app/CreateAccountActivity.java
@@ -2,9 +2,11 @@ package com.example.juangui.un_app;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,59 +17,37 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
-
+import android.support.v7.app.ActionBarDrawerToggle;
 public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText user,pass,pass2,license;
-    CheckBox car,moto;
+    EditText pass,pass2,license;
     private String FIREBASE_URL="https://unapp-c52f0.firebaseio.com";
     Firebase firebase;
-    int vehi;
     Button botoncreate,botoncancelar;
-    String name;
-    String passw;
-    String passw2;
-    String License;
     View v;
-    boolean Constante=true;
+    String texto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
         firebase.setAndroidContext(this);
         firebase =new Firebase(FIREBASE_URL);
-        user=(EditText) findViewById(R.id.user);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(mToolbar);
+        mToolbar.setTitle("Cambiar contraseña");
+        //mToolbar.setSubtitle("Sub");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
         pass=(EditText) findViewById(R.id.pass);
         pass2=(EditText) findViewById(R.id.pass2);
-        license=(EditText) findViewById(R.id.license);
-        car=(CheckBox) findViewById(R.id.car);
-        moto=(CheckBox) findViewById(R.id.moto);
-        botoncreate=(Button) findViewById(R.id.button2);
+        botoncreate=(Button) findViewById(R.id.crear);
         botoncancelar=(Button) findViewById(R.id.cancelar);
         botoncreate.setOnClickListener(this);
         botoncancelar.setOnClickListener(this);
-        license.setVisibility(v.INVISIBLE);
-        car.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(car.isChecked() || moto.isChecked()){
-                    license.setVisibility(v.VISIBLE);
-                }else{
-                    license.setVisibility(v.INVISIBLE);
-                }
-            }
-        });
 
-        moto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(moto.isChecked() || car.isChecked()){
-                    license.setVisibility(v.VISIBLE);
-                }else{
-                    license.setVisibility(v.INVISIBLE);
-                }
-            }
-        });
     }
 
     @Override
@@ -78,128 +58,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                 startActivity(intent);
                 break;
 
-
-            case R.id.button2:
-                name=user.getText().toString();
-                passw=pass.getText().toString();
-                passw2=pass2.getText().toString();
-                License=license.getText().toString();
-
-
-                if(name.equals("")){
-                    AlertDialog.Builder alerta = new AlertDialog.Builder(v.getContext());
-                    alerta.setMessage("El usuario es requerido")
-                            .setCancelable(false)
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.cancel();
-                                }
-                            });
-                    AlertDialog alert = alerta.create();
-                    alert.setTitle("Alerta");
-                    alert.show();
-                }
-                else if(passw.equals("") || passw2.equals("")){
-                    AlertDialog.Builder alerta = new AlertDialog.Builder(v.getContext());
-                    alerta.setMessage("La contraseña es requerida")
-                            .setCancelable(false)
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.cancel();
-                                }
-                            });
-                    AlertDialog alert = alerta.create();
-                    alert.setTitle("Alerta");
-                    alert.show();
-                }
-                else if(License.equals("") && license.getVisibility()== v.VISIBLE){
-                    AlertDialog.Builder alerta = new AlertDialog.Builder(v.getContext());
-                    alerta.setMessage("La placa es requerida")
-                            .setCancelable(false)
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.cancel();
-                                }
-                            });
-                    AlertDialog alert = alerta.create();
-                    alert.setTitle("Alerta");
-                    alert.show();
-                }
-                else {
-                    if (passw.equals(passw2)) {
-                        vehi = 0;
-                        if (car.isChecked()) {
-                            vehi++;
-                        }
-                        if (moto.isChecked()) {
-                            vehi += 2;
-                            license.setVisibility(v.VISIBLE);
-                        }
-
-
-                            firebase.addValueEventListener(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(DataSnapshot snapshot) {
-
-                                    if (snapshot.hasChild(name) && Constante) {
-
-                                        AlertDialog.Builder alerta = new AlertDialog.Builder(v.getContext());
-                                        alerta.setMessage("El usuario ya existe")
-                                                .setCancelable(false)
-                                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                                    @Override
-                                                    public void onClick(DialogInterface dialog, int which) {
-                                                        dialog.cancel();
-                                                    }
-                                                });
-                                        AlertDialog alert = alerta.create();
-                                        alert.setTitle("Alerta");
-                                        alert.show();
-                                    } else {
-
-                                        Constante=false;
-                                        firebase = new Firebase(FIREBASE_URL).child(name);
-                                        firebase.child("password").setValue(passw);
-                                        firebase.child("vehicle").setValue(vehi);
-                                        firebase.child("license").setValue(License);
-                                        firebase.child("Service").setValue("");
-                                        Toast.makeText(v.getContext(), "Cuenta creada!!", Toast.LENGTH_SHORT).show();
-                                        int x = 1;
-                                        while (x < 10000) {
-                                            x++;
-                                        }
-                                        Intent intent = new Intent(v.getContext(), LoginActivity.class);
-                                        v.getContext().startActivity(intent);
-                                    }
-
-                                }
-
-                                @Override
-                                public void onCancelled(FirebaseError firebaseError) {
-
-                                }
-                            });
-                        }
-
-                    else {
-                        AlertDialog.Builder alerta = new AlertDialog.Builder(v.getContext());
-                        alerta.setMessage("Las contraseñas no coinciden")
-                                .setCancelable(false)
-                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.cancel();
-                                    }
-                                });
-                        AlertDialog alert = alerta.create();
-                        alert.setTitle("Alerta");
-                        alert.show();
-                    }
-                }
-
+            case R.id.crear:
                 break;
 
 
